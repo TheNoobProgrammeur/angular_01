a1f59362db729564b9f05f0ce839fa28ea0382a3
Berthier Antoine <aberthier15@gmail.com>
2020-02-10 20:38:32 +0100

    Ajout des req API

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 
+const axios = require('axios');
 const routes = Router();
 
 /**
@@ -32,5 +33,93 @@ routes.get('/list', (req, res, next) => {
 
   res.render('index', { title });
 });
+
+routes.get('/hi', async (req, res ) => {
+  let catFact = await  catFactGet() ;
+  let jockGet = await JockGet();
+  let pointGeo = await geoPoint();
+  let beerget = await  beerGet();
+  let tacos = await tacosGet();
+  // data =  JockGet(data);
+  return res.send(  {
+    catFact,
+    joke:jockGet,
+    gps:{
+      lat: pointGeo[0],
+      lgn: pointGeo[1],
+    },
+    beer: {
+      name: beerget.name,
+      description: beerget.description
+    },
+    tacos,
+  }  );
+});
+
+
+async function catFactGet() {
+  try {
+    let  { data } = await  axios.get('https://cat-fact.herokuapp.com/facts/random',{
+      params: {
+        amount: 3
+      }
+    });
+    return [data[0].text, data[1].text, data[2].text]
+  } catch (e) {
+    return "Error in request "
+  }
+
+}
+
+async function JockGet() {
+  try {
+    let { data } = await axios.get('https://sv443.net/jokeapi/v2/joke/Programming');
+
+    if(data.joke){
+      return data.joke
+    }
+
+    return [data.setup, data.delivery]
+  } catch (e) {
+    return "Error in request "
+  }
+
+}
+
+async  function geoPoint() {
+  try {
+    let {data} = await  axios.get('https://api-adresse.data.gouv.fr/search/',
+      {
+        params: {
+          q:"41 rue du Port",
+          limit:1
+        }
+      });
+    return data.features['0'].geometry.coordinates ;
+  } catch (e) {
+    return "Error in request"
+  }
+
+}
+
+async function beerGet() {
+  try {
+    let {data} = await axios.get('https://api.punkapi.com/v2/beers/random');
+    return data[0]
+  } catch (e) {
+    return "Error in request"
+  }
+
+}
+
+async  function tacosGet() {
+  try {
+    let {data} = await  axios.get('http://taco-randomizer.herokuapp.com/random/')
+    return data.shell.name
+  }catch (e) {
+    return "Error in request"
+  }
+
+}
 
 export default routes;
